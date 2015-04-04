ae36d20738468e18b34100b737883571349a19ff
李倍存-2 <1174751315@qq.com>
2015-04-04 23:15:23 +0800

    【特性】 1  自动发送包含附件的日志邮件； 2  每日定时任务包括同步气象、负荷数据以及发送日志自动邮件；

--- src/test/java/loadPrediction/action/PredictionActionTest.java
+++ src/test/java/loadPrediction/action/PredictionActionTest.java
@@ -3,6 +3,13 @@ package loadPrediction.action;
 import junit.framework.TestCase;
 import loadPrediction.log.Logging;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.mail.MailMessage;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
 
 public class PredictionActionTest extends TestCase {
 
@@ -12,6 +19,8 @@ public class PredictionActionTest extends TestCase {
             action.setUseCaches(false);
             action.setDateString("2014-04-22");
             action.intelli();
+
+
 //        Logging.instance().createLogger("INFO").info("INFO");
 //        Logging.instance().createLogger("ERR").error("ERROR");
 //        Logging.instance().createLogger("DEBUG").debug("DBG");
