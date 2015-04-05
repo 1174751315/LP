bf64adb77454cc4119d2f39fe9bfced12ab96713
李倍存-2 <1174751315@qq.com>
2015-04-05 09:18:50 +0800

    【微调】 1  在用户发送问题报告之后，定位到成功或失败的页面；

--- src/main/java/loadPrediction/action/ProblemReportingAction.java
+++ src/main/java/loadPrediction/action/ProblemReportingAction.java
@@ -1,9 +1,14 @@
 package loadPrediction.action;
 
 import com.opensymphony.xwork2.ActionSupport;
+import loadPrediction.exception.LPE;
+import loadPrediction.log.Logging;
 import loadPrediction.utils.LogMailingUtils;
+import org.apache.log4j.Logger;
+import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -25,12 +30,31 @@ public class ProblemReportingAction extends ActionSupport {
     }
 
     public String sendEMail() throws Exception {
-        JavaMailSenderImpl sender= LogMailingUtils.getDefaultSender();
-        MimeMessage message=LogMailingUtils.createMimeMessage(sender);
-        String text="用户报告了一个问题。\n【描述】\n"+details+"\n";
-        text+="【时间】"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())+"\n";
-        message.setText(text);
-        sender.send(message);
+        try {
+            JavaMailSenderImpl sender= LogMailingUtils.getDefaultSender();
+            MimeMessage message=LogMailingUtils.createMimeMessage(sender);
+            String text="用户报告了一个问题。\n【描述】\n"+details+"\n";
+            text+="【时间】"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())+"\n";
+            message.setText(text);
+            sender.send(message);
+        } catch (LPE lpe) {
+            lpe.printStackTrace();
+            return failed(lpe.getMessage());
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return failed(e.getMessage());
+        } catch (MailException e) {
+            e.printStackTrace();
+            return failed(e.getMessage());
+        }
+
         return SUCCESS;
+    }
+    private String failed(String msg){
+        Logger log=Logging.instance().createLogger("问题报告");
+        String inf="用户在发送问题报告时出现问题。\n";
+        log.error(inf + msg);
+        log.info(inf);
+        return ERROR;
     }
 }
