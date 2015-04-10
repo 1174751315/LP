b764758ea66ae062450f4d439200a0f650b7a661
李倍存-2 <1174751315@qq.com>
2015-04-10 13:18:46 +0800

    【备份】 1  新增mockito的maven依赖项；

---
+++ src/main/java/learning/mockito/TestMockito.java
@@ -0,0 +1,28 @@
+package learning.mockito;
+
+import loadPrediction.core.predictor.IPredictor;
+import loadPrediction.exception.LPE;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 李倍存 创建于 2015-04-09 20:54。电邮 1174751315@qq.com。
+ */
+public class TestMockito {
+    public TestMockito() {
+    }
+
+    @Test
+    public void test()throws Exception{
+        IPredictor predictor=mock(IPredictor.class);
+
+        when(predictor.getDateString()).thenReturn("2014-04-21");
+        when(predictor.predict()).thenThrow(new LPE());
+
+        predictor.predict();
+    }
+
+}
---
+++ src/test/java/loadPrediction/domain/LoadDataTest.java
@@ -0,0 +1,13 @@
+package loadPrediction.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoadDataTest {
+
+    @Test
+    public void testAccept() throws Exception {
+
+    }
+}
