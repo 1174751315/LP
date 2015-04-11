cd06772ad9ed0bee5d62be219e79299514a1de1a
李倍存-9楼 <1174751315@qq.com>
2015-04-11 12:17:29 +0800

    【备份】

--- src/main/java/loadPrediction/core/predictor/excelling/ExcellingWorkdayPredictor.java
+++ src/main/java/loadPrediction/core/predictor/excelling/ExcellingWorkdayPredictor.java
@@ -99,8 +99,8 @@ public class ExcellingWorkdayPredictor extends AbstractTemplateMethodExcellingPr
 
     @Override
     protected String doGetInputWorkbookPath() {
-        if (season.equals(Season.SUMMER))
-            return IOPaths.WEB_CONTENT_WORKDAY_SUMMER_TEMPLATE_PATH;
+//        if (season.equals(Season.SUMMER))
+//            return IOPaths.WEB_CONTENT_WORKDAY_SUMMER_TEMPLATE_PATH;
         return IOPaths.WEB_CONTENT_WORKDAY_WINTER_TEMPLATE_PATH;
     }
     @Override
